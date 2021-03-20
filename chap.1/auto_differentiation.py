66db744a63a61b5fda8b8e6378c5bbf50f8fe447
hiki-masa <hikimasa.micro@gmail.com>
2021-03-20 18:28:23 +0900

    A little fix
    微分計算の実行をより分かりやすく改良．f(x)の極値でf'(x)の値が0になっているなど，微分が行われているのが確認できる．

--- chap.1/auto_differentiation.py
+++ chap.1/auto_differentiation.py
@@ -1,16 +1,24 @@
 import torch
+import matplotlib.pyplot as plt
 
 # 関数
 def f(x):
-    return (2 * x**3) + (4 * x**2) + (5 * x) + 6
+    return (2 * x**3) - (3 * x**2) - (12 * x) + 3
 
 # requires_grad = True で微分の変数の設定
-x = torch.tensor(0.5, requires_grad = True)
+x = torch.arange(-4, 4, 0.01, requires_grad = True)
 # 計算することで計算グラフの作成を行う
 y = f(x)
 # 微分の実行
-y.backward()
+y.backward(gradient = torch.ones_like(y))   # 複数のスカラー値の場合，勾配を格納する場所が必要なので，テンソルを仮生成
 # 勾配の計算
 g = x.grad
 
-print(g)
+# グラフの作成・設定
+plt.plot(x.detach().numpy(), y.detach().numpy(), label = "f(x)")
+plt.plot(x.detach().numpy(), g.detach().numpy(), label = "f'(x)")
+plt.xlim(-4, 4)
+plt.ylim(-30, 30)
+plt.grid()
+plt.legend()
+plt.show()
